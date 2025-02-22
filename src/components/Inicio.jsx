6fdd6460d3a66c69d8ffc3eab44641b4fce86870
oriana-monaldi <orimonaldi@gmail.com>
2025-02-22 03:30:31 -0300

    imagenes inicio

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import car7 from "../img/car7.jpg";
-import logo from "../img/logo.png";
+import car4 from "../img/car4.jpg";
 import ingresoReal from "../img/ingreso_real.jpg";
+import logo from "../img/logo.png";
 import Navbar from "./Navbar";
 import Counter from "./Counter";
 
+const images = [ingresoReal, car7, car4];
+
 function Inicio() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 4000); 
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
-      <div className="relative flex items-center justify-center">
-        <img
-          src={car7}
-          alt="car7"
-          className="w-full h-[40vh] object-cover opacity-30"
-        />
+      <div className="relative flex items-center justify-center overflow-hidden h-[40vh]">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`slide-${index}`}
+            className={`absolute w-full h-full object-cover transition-opacity duration-1000 ${
+              index === currentIndex ? "opacity-30 z-10" : "opacity-0 z-0"
+            }`}
+          />
+        ))}
         <Navbar />
         <img
           src={logo}
           alt="logo"
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-60 object-contain"
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-60 object-contain z-20"
         />
       </div>
       <div className="relative">
@@ -59,9 +76,9 @@ function Inicio() {
         alt="ingresoReal"
         className="w-[340px] h-[250px] rounded-2xl mx-auto mt-4"
       />
-    <div>
-      <Counter/>
-    </div>
+      <div>
+        <Counter />
+      </div>
     </div>
   );
 }
