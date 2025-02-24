e96e0dea3af9a21f0cf87a753c664295a8eef392
oriana-monaldi <orimonaldi@gmail.com>
2025-02-23 21:39:01 -0300

    msc

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import { Clock, Phone, MapPin } from 'lucide-react';
-import SocialIcons from './SocialIcons';
-import fondoServicios from '../img/fondoServicios.jpg';  
-
 const Contacto = () => {
   return (
     <div 
       id="contacto" 
-      className="bg-cover bg-center relative"  
+      className="relative bg-cover bg-center"  
       style={{ 
         backgroundImage: `url(${fondoServicios})`, 
-        backgroundAttachment: 'fixed',  // Esto hace que el fondo tenga el efecto parallax
+        backgroundAttachment: 'fixed', 
         paddingTop: '50px', 
-        paddingBottom: '50px', 
+        paddingBottom: '50px',
+        backgroundPosition: 'center',
       }}
     >
-      {/* Capa opaca encima de la imagen */}
-      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50 z-0"></div>  
+      <div className="absolute inset-0 bg-black opacity-50 z-0"></div>  
 
       <div className="max-w-6xl mx-auto px-4 lg:w-4/5 relative z-10">
         <h1 className="text-white text-5xl p-8 mb-6 text-center font-[Sprit] w-full">
@@ -28,8 +23,8 @@ const Contacto = () => {
               <Clock className="w-6 h-6 mb-4 text-blue-400" />
               <h3 className="text-xl font-semibold mb-4 text-white">Horarios de Atención</h3>
             </div>
-            <div className="space-y-2 ">
-              <p className="flex justify-between text-white mb-4 ">
+            <div className="space-y-2">
+              <p className="flex justify-between text-white mb-4">
                 <span>Lunes a Viernes:</span>
                 <span>8:00 - 18:00</span>
               </p>
@@ -39,9 +34,9 @@ const Contacto = () => {
               </p>
             </div>
           </div>
-    
+
           <div className="bg-white/5 backdrop-blur-sm rounded-lg p-4">
-            <div className="flex items-center gap-3 mb-2 ">
+            <div className="flex items-center gap-3 mb-2">
               <Phone className="w-6 h-6 mt-2 ml-2 text-yellow-400" />
               <h3 className="text-xl font-semibold mt-2 text-white">Medio de contacto</h3>
             </div>
@@ -49,13 +44,12 @@ const Contacto = () => {
           </div>
         </div>
 
-        {/* Mapa dentro de una tarjeta, más largo */}
         <div className="bg-white/5 backdrop-blur-sm rounded-lg p-6 mb-12">
           <div className="flex items-center gap-2 justify-center mb-8">
             <MapPin className="w-6 h-6 text-red-500" />
             <h3 className="text-2xl font-semibold text-white">Nuestra Ubicación</h3>
           </div>
-          <div className="w-full rounded-lg overflow-hidden ">
+          <div className="w-full rounded-lg overflow-hidden">
             <iframe 
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.7514268020955!2d-57.95878969842563!3d-34.887587767286085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e5ff9f72bde5%3A0x506d278c3b875a4b!2sTaller%20AltaGama!5e0!3m2!1ses-419!2sar!4v1740263236766!5m2!1ses-419!2sar" 
               width="100%" 
@@ -64,7 +58,7 @@ const Contacto = () => {
               allowFullScreen="" 
               loading="lazy" 
               referrerPolicy="no-referrer-when-downgrade"
-              className="rounded-lg "
+              className="rounded-lg"
             />
           </div>
         </div>
@@ -72,5 +66,3 @@ const Contacto = () => {
     </div>
   );
 };
-
-export default Contacto;
